8c81d1a840064e83562c1cb65415f6efd04cd043
EhsanJaved <ehsanjavedrock@gmail.com>
2022-10-02 19:29:02 +0500

    first part comp 1 error is list dot still visible

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,15 +1,17 @@
 // import logo from './logo.svg';
 import {Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/menuComponents';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
         <Navbar dark color='primary'>
           <div className='container'>
             <NavbarBrand href="/"> the new app </NavbarBrand>
           </div>
         </Navbar>
+        <Menu/>
  
 
     </div>
